f891bd1c3946753bc41773c657376119cccf3339
Kevin Wyckmans <kevin@wyckd.be>
2018-07-25 23:24:23 +0200

    reference coveralls plugin explicitly

--- src/test/be/wyckd/datastructures/HashmapTest.java
+++ src/test/be/wyckd/datastructures/HashmapTest.java
@@ -1,13 +1,8 @@
 package be.wyckd.datastructures;
 
 import be.wyckd.util.Person;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.security.Key;
-
-import static org.junit.Assert.*;
 
 public class HashmapTest {
     private Hashmap<String, Person> map;
